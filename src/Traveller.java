3c79796c494cf90b74b261af8deb01942009ddf0
NADO1995 <tdorji22@myune.edu.au>
2026-08-31 09:26:51 +1000

    Refactored Traveller as client record

--- src/Traveller.java
+++ src/Traveller.java
@@ -1,26 +1,8 @@
-public class Traveller {
-
-    private final String name;
-    private final String email;
-    private final String phone;
-
-    public Traveller(String name, String email, String phone) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
+public record Traveller(
+        String name,
+        String email,
+        String phone
+) {
 
     public String getTravellerInformation() {
         return "Name: " + name
