984d85d84a3629eae582591d8541271d78daa9f9
NADO1995 <tdorji22@myune.edu.au>
2026-08-31 09:23:05 +1000

    Added experience search methods to registry

--- src/ExperienceRegistry.java
+++ src/ExperienceRegistry.java
@@ -16,4 +16,28 @@ public class ExperienceRegistry {
     public List<Experience> getExperiences() {
         return new ArrayList<>(experiences);
     }
+
+    public List<Experience> findMatchingExperiences(DreamExperience dream) {
+
+        List<Experience> matches = new ArrayList<>();
+
+        for (Experience experience : experiences) {
+            if (experience.matches(dream)) {
+                matches.add(experience);
+            }
+        }
+
+        return matches;
+    }
+
+    public Experience findById(int id) {
+
+        for (Experience experience : experiences) {
+            if (experience.getId() == id) {
+                return experience;
+            }
+        }
+
+        return null;
+    }
 }
